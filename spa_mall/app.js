fa719f88f53afedabbf91e24dd6dca146e89f526
fancyers <kjng5901@gmail.com>
2022-01-22 18:21:23 +0900

    Node.js 기초반 3주차
    변경사항 : DB 연결, 장바구니 구현

--- spa_mall/app.js
+++ spa_mall/app.js
@@ -1,18 +1,22 @@
 const express = require('express')
+const connect = require('./schemas')
 const app = express()
 const port = 3000
 
+connect()
+
 const goodsRouter = require('./routes/goods')
+const cartsRouter = require('./routes/carts')
 
 // 미들웨어
 const requestMiddleware = (req, res, next) => {
     console.log('Request URL:', req.originalUrl, ' - ', new Date())
     next()
 }
-
+app.use(express.json())
 app.use(requestMiddleware)
 
-app.use('/api', goodsRouter)
+app.use('/api', [goodsRouter, cartsRouter])
 
 
 // request, respond
--- spa_mall/routes/goods.js
+++ spa_mall/routes/goods.js
@@ -1,58 +1,82 @@
 const express = require('express')
+const Goods = require('../schemas/goods')
+const Cart = require('../schemas/cart')
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.send('this is root page')
 })
 
-const goods = [
-    {
-      goodsId: 4,
-      name: "상품 4",
-      thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-      category: "drink",
-      price: 0.1,
-    },
-    {
-      goodsId: 3,
-      name: "상품 3",
-      thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-      category: "drink",
-      price: 2.2,
-    },
-    {
-      goodsId: 2,
-      name: "상품 2",
-      thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-      category: "drink",
-      price: 0.11,
-    },
-    {
-      goodsId: 1,
-      name: "상품 1",
-      thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-      category: "drink",
-      price: 6.2,
-    },
-  ];
+router.get('/goods', async (req, res) => {
+    const { category } = req.query
 
-router.get('/goods', (req, res) => {
+    const goods = await Goods.find({ category })
     res.json({
         goods
     })
 })
 
-router.get('/goods/:goodsId', (req, res) => {
-    const goodsId = req.params.goodsId
-    
-    const [detail] = goods.filter((item) => item.goodsId === Number(goodsId))
+router.get('/goods/:goodsId', async (req, res) => {
+    const { goodsId } = req.params
+
+    const [detail] = await Goods.find({ goodsId: Number(goodsId) })
+
     res.json({
         detail
     })
+})
+
+//  장바구니에 추가
+router.post('/goods/:goodsId/cart', async (req, res) => {
+    const { goodsId } = req.params
+    const { quantity } = req.body
+
+    const existsCarts = await Cart.find({ goodsId: Number(goodsId) })
+    if (existsCarts.length) {
+        return res.status(400).json({ success: false, errorMessage: '이미 장바구니에 들어있는 상품입니다.' })
+    }
+
+    await Cart.create({ goodsId: Number(goodsId), quantity })
+    res.json({ success: true })
+})
+
+//  장바구니에서 삭제
+router.delete('/goods/:goodsId/cart', async (req, res) => {
+    const { goodsId } = req.params
+
+    const existsCarts = await Cart.find({ goodsId: Number(goodsId) })
+    if (existsCarts.length) {
+        await Cart.deleteOne({ goodsId: Number(goodsId) })
+    }
+    res.json({ success: true })
+})
+
+//  상품 수량 변경
+router.put('/goods/:goodsId/cart', async (req, res) => {
+    const { goodsId } = req.params
+    const { quantity } = req.body
+
+    const existsCarts = await Cart.find({ goodsId: Number(goodsId) })
+    if (!existsCarts.length) {
+        return res.status(400).json({ success: false, errorMessage: '장바구니에 해당 상품이 없습니다.' })
+    }
+
+    await Cart.updateOne({ goodsId: Number(goodsId) }, { $set: { quantity } })
+
+    res.json({ success: true })
+})
+
+router.post('/goods', async (req, res) => {
+    const { goodsId, name, thumbnailUrl, category, price } = req.body
+
+    const goods = await Goods.find({ goodsId });
+    if (goods.length) {
+        return res.status(400).json({ success: false, errorMessage: '이미 있는 데이터입니다.' })
+    }
+
+    const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price })
+
+    res.json({ goods })
 })
 
 module.exports = router
